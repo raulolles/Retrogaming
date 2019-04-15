66cf4977f95ff4bbc2bef2e6fca62ad0bea92789
raulolles <33615301+raulolles@users.noreply.github.com>
2019-04-15 13:21:35 +0200

    #

--- app/routes.py
+++ app/routes.py
@@ -19,7 +19,7 @@ origen_datos = "app/static/datos/"
 # Si el usuario está logeado selecciona ofertas
 def index():
 	global selec
-	select.clear()
+	selec.clear()
 	txt = control_lenguaje(request.args)
 	id_user = current_user.id - 1
 	select_modelos, select_users, select_juegos = select_predicciones(origen_datos, id_user)
@@ -63,7 +63,7 @@ def login():
 # Si el usuario está logeado selecciona ofertas
 def favoritos():
 		global selec
-		select.clear()
+		selec.clear()
 		txt = control_lenguaje(request.args)
 		id_user = current_user.id - 1
 		select_fav = select_favoritos(origen_datos, id_user)
@@ -458,7 +458,7 @@ def mas_comments_archive2_no_jugado():
 @login_required
 def busqueda():
 	global selec
-	select.clear()
+	selec.clear()
 	txt = control_lenguaje(request.args)
 	id_user = current_user.id - 1
 	palabra_busq = request.args.get('q')
